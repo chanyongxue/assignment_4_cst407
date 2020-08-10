27720a23417facd47a5adf72258a2782d919ed56
Courtney Chan <chanyongxue@users.noreply.github.com>
2020-08-09 22:54:40 -0700

    Update like_state.dart

--- lib/state/like_state.dart
+++ lib/state/like_state.dart
@@ -1,4 +1,5 @@
 import 'package:assignment_4_cst407/screens/favoriteanime.dart';
+import 'package:assignment_4_cst407/widgets/favoritebutton.dart';
 import 'package:riverpod/riverpod.dart';
 
-final likeProvider = StateProvider((ref) => FavoriteAnime());
+final likeProvider = StateProvider((ref) => FavoriteButton());
