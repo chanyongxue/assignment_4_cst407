be3f2e18b01d7292711040a521cfa1be9c4806b7
Courtney Chan <chanyongxue@users.noreply.github.com>
2020-08-10 13:37:14 -0700

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,7 +12,7 @@ import 'package:assignment_4_cst407/auth/auth_methods.dart';
 
 class Home extends StatelessWidget {
   final AuthMethods _auth = AuthMethods();
-  
+
   Widget _homeBuild() {
     return ListView(
       children: <Widget>[
@@ -72,7 +72,7 @@ class Home extends StatelessWidget {
             subtype: TopSubtype.special,
           ),
         ),
-         Padding(
+        Padding(
           padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
           child: Text(
             "airing",
@@ -97,75 +97,68 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var _homeWidgets = <Widget>[
-    _homeBuild(),
-    FavoriteAnimeList(),
-    Profile(),
-  ];
+      _homeBuild(),
+      FavoriteAnimeList(),
+      Profile(),
+    ];
     return Consumer((context, watch) {
       final index = watch(selectedIndex).state;
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          centerTitle: true,
-          title: Text("my anime list",
-          style: TextStyle(
-            fontSize: 25,),
+      return WillPopScope(
+        onWillPop: () => Future.value(false),
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.black,
+            centerTitle: true,
+            title: Text(
+              "my anime list",
+              style: TextStyle(
+                fontSize: 25,
+              ),
             ),
-          automaticallyImplyLeading: false,
-          actions: <Widget>[
-            FlatButton.icon(
-              icon: Icon(Icons.person_outline, 
-              color: Colors.white),
-              label: Text('log out',
-              style: TextStyle(color: Colors.white)),
-              onPressed: () async {
-                await _auth.signOut();
-                Navigator.pushReplacementNamed(context, '/wrapper');
-              },
-            ),
-          ],
-        ),
-        bottomNavigationBar: Container(
-          decoration: BoxDecoration(color: Colors.grey[850], boxShadow: [
-            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
-          ]),
-          child: SafeArea(
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-              child: GNav(
-                  gap: 8,
-                  activeColor: Colors.white,
-                  iconSize: 24,
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                  duration: Duration(milliseconds: 200),
-                  tabBackgroundColor: Colors.black,
-                  tabs: [
-                    GButton(
-                      icon: LineIcons.home,
-                      iconColor: Colors.white,
-                      text: 'home',
-                    ),
-                    GButton(
-                      icon: LineIcons.heart_o,
-                      iconColor: Colors.white,
-                      text: 'likes',
-                    ),
-                    GButton(
+          ),
+          bottomNavigationBar: Container(
+            decoration: BoxDecoration(color: Colors.grey[850], boxShadow: [
+              BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+            ]),
+            child: SafeArea(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+                child: GNav(
+                    gap: 8,
+                    activeColor: Colors.white,
+                    iconSize: 24,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    duration: Duration(milliseconds: 200),
+                    tabBackgroundColor: Colors.black,
+                    tabs: [
+                      GButton(
+                        icon: LineIcons.home,
+                        iconColor: Colors.white,
+                        text: 'home',
+                      ),
+                      GButton(
+                        icon: LineIcons.heart_o,
+                        iconColor: Colors.white,
+                        text: 'likes',
+                      ),
+                      GButton(
                         icon: LineIcons.user,
                         text: 'profile',
                         iconColor: Colors.white,
-                      ), 
-                  ],
-                  selectedIndex: index,
-                  onTabChange: (index) {
-                    selectedIndex.read(context).state = index;
-                  }),
+                      ),
+                    ],
+                    selectedIndex: index,
+                    onTabChange: (index) {
+                      selectedIndex.read(context).state = index;
+                    }),
+              ),
             ),
           ),
+          body: _homeWidgets[index],
+          backgroundColor: Colors.black,
         ),
-        body: _homeWidgets[index],
-        backgroundColor: Colors.black,
       );
     });
   }
