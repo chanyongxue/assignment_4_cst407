8209cdd3c1fdf564af6c3a04e6a179d27fb6e1bf
Courtney Chan <chanyongxue@users.noreply.github.com>
2020-08-11 10:06:56 -0700

    Update auth_methods.dart

--- lib/auth/auth_methods.dart
+++ lib/auth/auth_methods.dart
@@ -1,10 +1,14 @@
 import 'package:assignment_4_cst407/models/user.dart';
+import 'package:assignment_4_cst407/state/database_state.dart';
 import 'package:assignment_4_cst407/state/user_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<FirebaseUser> get currentFirebaseUser async =>
+      await _auth.currentUser();
 
   Future signOut() async {
     try {
@@ -15,10 +19,18 @@ class AuthMethods {
     }
   }
 
-  Future<String> registerWithEmailAndPassword(String email, String password) async {
+  Future<String> registerWithEmailAndPassword(
+      String email, String password) async {
     try {
-      await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      return await signIn(email, password);
+      await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      var result = await signIn(email, password);
+      var currentUser = await currentFirebaseUser;
+
+      databaseProvider.readOwner(ProviderStateOwner()).state.addUser(
+          currentUser.uid,
+          {'uid': currentUser.uid, 'email': currentUser.email});
+      return result == null ? result : throw result;
     } catch (e) {
       return (e).toString();
     }
@@ -26,10 +38,12 @@ class AuthMethods {
 
   Future<String> signIn(String email, String password) async {
     try {
-      AuthResult result = await _auth.signInWithEmailAndPassword(
+      await _auth.signInWithEmailAndPassword(
           email: email, password: password);
-      FirebaseUser user = result.user;
-      userProvider.readOwner(ProviderStateOwner()).state = User(uid: user.uid);
+          //TODO: FIX USER PROVIDER SETTERRRRR
+      var user = userProvider.readOwner(ProviderStateOwner()).state;
+      user.uid = await currentFirebaseUser.then((user) => user.uid);
+      user.email = email;
       return null;
     } catch (e) {
       return (e).toString();
