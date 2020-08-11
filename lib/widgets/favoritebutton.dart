68233e4fecd57f362ce78db2fe0e63a632355762
Courtney Chan <chanyongxue@users.noreply.github.com>
2020-08-11 10:07:21 -0700

    Update favoritebutton.dart

--- lib/widgets/favoritebutton.dart
+++ lib/widgets/favoritebutton.dart
@@ -1,10 +1,14 @@
+import 'package:assignment_4_cst407/state/auth_state.dart';
+import 'package:assignment_4_cst407/state/database_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jikan_api/jikan_api.dart';
 
 class FavoriteButton extends StatefulWidget {
-  FavoriteButton({Key key, this.top}) : super(key: key);
+  FavoriteButton({Key key, this.top, this.isLiked}) : super(key: key);
 
   final Top top;
+  final bool isLiked;
 
   @override
   _FavoriteButtonState createState() => _FavoriteButtonState();
@@ -14,6 +18,12 @@ class _FavoriteButtonState extends State<FavoriteButton> {
   //TODO: set isLiked to database
   var isLiked = false;
   @override
+  void initState() async {
+    super.initState();
+    isLiked = widget.isLiked;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return IconButton(
         icon: Icon(
@@ -21,6 +31,15 @@ class _FavoriteButtonState extends State<FavoriteButton> {
           color: isLiked ? Colors.redAccent : Colors.grey,
           size: 30,
         ),
-        onPressed: () => setState(() => isLiked = !isLiked));
+        onPressed: () async {
+          databaseProvider.read(context).state.updateUserLikes(
+              await authProvider
+                  .read(context)
+                  .state
+                  .currentFirebaseUser
+                  .then((user) => user.uid),
+              widget.top.malId);
+          setState(() => isLiked = !isLiked);
+        });
   }
 }
