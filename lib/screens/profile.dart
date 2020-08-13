184603662248d7fa2fca81eae580fa20d8d811ce
Courtney Chan <chanyongxue@users.noreply.github.com>
2020-08-12 22:53:37 -0700

    Update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -7,19 +7,33 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.black,
-        alignment: Alignment.center,
-                child: FlatButton.icon(
-                  icon: Icon(Icons.person_outline), 
-                  color: Colors.white,
-                  label: Text('log out',
-                  style: TextStyle(color: Colors.black)),
-                   onPressed: () async {
-                    await authProvider.readOwner(ProviderStateOwner()).state.signOut();
-                    Navigator.pushReplacementNamed(context, '/wrapper');
-                },
-      ),
-
+      color: Colors.black,
+      alignment: Alignment.center,
+        child: ListView(
+          children: <Widget>[
+            Divider(color: Colors.transparent),
+            CircleAvatar(
+              radius: 60,
+              backgroundColor: Colors.white,
+              child: Icon(
+                Icons.person, 
+                size: 80,
+                color: Colors.black
+                ),
+            ),
+            Divider(color: Colors.transparent),
+          FlatButton.icon(
+                      icon: Icon(Icons.person_outline), 
+                      color: Colors.white,
+                      label: Text('log out',
+                      style: TextStyle(color: Colors.black)),
+                       onPressed: () async {
+                        await authProvider.readOwner(ProviderStateOwner()).state.signOut();
+                        Navigator.pushReplacementNamed(context, '/wrapper');
+                    },
+          ),
+          ],
+        ),
     );
   }
 }
